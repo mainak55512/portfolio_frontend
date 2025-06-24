376686203564c1f08a66ed368f788f3ddbee1eba
mainak55512 <mbhattacharjee432@gmail.com>
2025-06-25 00:14:28 +0530

    Added details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,16 @@
 	import { onMount } from 'svelte';
 	import { env } from '$env/dynamic/public';
 	interface Entity {
-		[key: string]: string;
+		name: string;
+		description: string;
+		path: string;
+		tech: string;
+		thumbnail: string;
 	}
 	interface Blog {
-		[key: string]: string;
+		title: string;
+		url: string;
+		thumbnail: string;
 	}
 	var entities: Entity[];
 	var blogs: Blog[];
@@ -43,15 +49,19 @@
 
 	<div class="grid grid-cols-4 gap-4">
 		{#each entities as entity}
-			<button
+			<a
 				class="isolate m-5 flex h-70 grow-1 flex-col rounded-xl bg-white/20 p-5 text-start text-white shadow-lg ring-1 ring-black/5 backdrop-blur-md"
+				href={entity.path}
+				target="_blank"
+				rel="noopener noreferrer"
+				data-sveltekit-reload
 			>
 				<div class="h-20">
 					<div class="text-lg font-semibold">{entity.name}</div>
 					<div class="text-xs text-white/50">{entity.description}</div>
 				</div>
-				<img alt="placeholder" src="/resource/placeholder.jpg" class="h-40 w-90 rounded-lg" />
-			</button>
+				<img alt={entity.name} src={entity.thumbnail} class="h-40 w-90 rounded-lg" />
+			</a>
 		{/each}
 	</div>
 	<span class="text-white/30">};</span><br />
@@ -59,15 +69,19 @@
 	<h1 class="mt-5 ml-5 text-left text-3xl font-black text-white/70">Blogs</h1>
 	<div class="grid grid-cols-4 gap-4">
 		{#each blogs as blog}
-			<button
+			<a
 				class="isolate m-5 flex h-70 grow-1 flex-col rounded-xl bg-white/20 p-5 text-start text-white shadow-lg ring-1 ring-black/5 backdrop-blur-md"
+				href={blog.url}
+				target="_blank"
+				rel="noopener noreferrer"
+				data-sveltekit-reload
 			>
 				<div class="h-20">
 					<div class="text-lg font-semibold">{blog.title}</div>
 					<!-- <div class="text-xs text-white/50">{entity.description}</div> -->
 				</div>
-				<img alt="placeholder" src="/resource/placeholder.jpg" class="h-40 w-90 rounded-lg" />
-			</button>
+				<img alt={blog.title} src={blog.thumbnail} class="h-40 w-90 rounded-lg" />
+			</a>
 		{/each}
 	</div>
 	<div class="flex flex-col justify-center">
@@ -76,8 +90,20 @@
 	<span class="text-white/30">return [</span>
 	<div class="flex justify-center">
 		<div class="flex flex-row">
-			<img alt="placeholder" src="/resource/github.png" class="m-2 h-8 w-8 rounded-lg" />
-			<img alt="placeholder" src="/resource/linkedin.png" class="m-2 h-8 w-8 rounded-lg" />
+			<a
+				href="https://github.com/mainak55512"
+				target="_blank"
+				rel="noopener noreferrer"
+				data-sveltekit-reload
+				><img alt="placeholder" src="/resource/github.png" class="m-2 h-8 w-8 rounded-lg" /></a
+			>
+			<a
+				href="https://www.linkedin.com/in/mainak-bhattacharjee-10773923a"
+				target="_blank"
+				rel="noopener noreferrer"
+				data-sveltekit-reload
+				><img alt="placeholder" src="/resource/linkedin.png" class="m-2 h-8 w-8 rounded-lg" /></a
+			>
 		</div>
 	</div>
 	<div class="flex flex-col justify-center">
